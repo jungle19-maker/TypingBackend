50db0bca6be909b86a4ae83f73a0329213009b28
tapswisonu <tapswisonu@github.com>
2026-01-21 18:51:36 +0530

    fix url

--- middleware/security.js
+++ middleware/security.js
@@ -36,7 +36,7 @@ const setupSecurity = (app) => {
 
     // CORS
     app.use(cors({
-        origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', process.env.CLIENT_URL],
+        origin: ['https://typingfrontend.vercel.app', 'https://typingadmin.vercel.app', process.env.CLIENT_URL],
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
         allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token'],
         credentials: true
